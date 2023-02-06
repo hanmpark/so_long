9c1d9523f55d00fa6de50f7d29589ba5476177a7
Hanmin Park <hanmin.01.park@gmail.com>
2023-02-06 18:14:45 +0100

    Starting with the mlx... wtf

--- src/main/game_init.c
+++ src/main/game_init.c
@@ -1,30 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   game_init.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hanmpark <hanmpark@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/11 10:22:52 by hanmpark          #+#    #+#             */
-/*   Updated: 2023/02/06 18:01:10 by hanmpark         ###   ########.fr       */
+/*   Created: 2023/02/06 17:28:52 by hanmpark          #+#    #+#             */
+/*   Updated: 2023/02/06 17:30:04 by hanmpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/so_long.h"
 
-int	main(int argc, char **argv)
+void	game_init(char **map, t_parse *mapi)
 {
-	char	**map;
-	t_parse	*mapi;
-
-	if (argc != 2 || !argv[1])
-		return (1);
-	mapi = malloc(sizeof(t_parse));
-	if (!mapi)
-		return (1);
-	map = map_init(argv[1], mapi);
-	//game_init(map, mapi);
-	free(mapi);
-	ft_freetab(map);
-	return (0);
+	
 }
